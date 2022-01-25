c0abc461488a97a829ccd90b1bc34b3856faf028
CH.Nithin Chakravarthy <94435704+Chakravarthy738@users.noreply.github.com>
2022-01-25 10:36:54 +0530

    Compute Uniform Probabilities

--- language.py
+++ language.py
@@ -127,7 +127,8 @@ Parameters: list of strs
 Returns: list of floats
 '''
 def buildUniformProbs(unigrams):
-    return
+    probs=[1/len(unigrams)]*len(unigrams)
+    return probs
 
 
 '''
@@ -323,26 +324,28 @@ def scatterPlot(xs, ys, labels, title):
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
-    print("\n" + "#"*15 + " WEEK 1 TESTS " +  "#" * 16 + "\n")
-    test.week1Tests()
-    print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
-    test.runWeek1()
+    #print("\n" + "#"*15 + " WEEK 1 TESTS " +  "#" * 16 + "\n")
+    #test.week1Tests()
+    #print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
+    #test.runWeek1()
     #test.testLoadBook()
     #test.testGetCorpusLength()
     #test.testBuildVocabulary()
     #test.testCountUnigrams()
     #test.testGetStartWords()
     #test.testCountStartWords()
-    test.testCountBigrams()
+    #test.testCountBigrams()
     
 
     ## Uncomment these for Week 2 ##
-"""
+
     print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
     test.week2Tests()
     print("\n" + "#"*15 + " WEEK 2 OUTPUT " + "#" * 15 + "\n")
     test.runWeek2()
-"""
+    test.testBuildUniformProbs()
+
+
 
     ## Uncomment these for Week 3 ##
 """
